adc708b88dd54b7b998746bc3838654721feb546
Gokul <gokul.j@msds.christuniversity.in>
2026-02-15 21:12:00 +0530

    updated app.py with port

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-class HealthHandler(BaseHTTPRequestHandler):
+class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/health":
             self.send_response(200)
             self.end_headers()
             self.wfile.write(b"OK")
         else:
-            self.send_response(404)
+            self.send_response(200)
             self.end_headers()
+            self.wfile.write(b"Python app running")
 
 def run():
-    server = HTTPServer(("0.0.0.0", 2026), HealthHandler)
-    print("Python app running on port 2026")
+    server = HTTPServer(("0.0.0.0", 8000), Handler)
+    print("Python app started on port 8000")
     server.serve_forever()
 
 if __name__ == "__main__":
