d043ac0112f830e2350ec632715235d8018eeab1
Gokul <gokul.j@msds.christuniversity.in>
2026-02-15 21:01:06 +0530

    updated app.py with port

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from DEV pipeline 🚀")
-    print("CodeBuild is running successfully")
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class HealthHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/health":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"OK")
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+def run():
+    server = HTTPServer(("0.0.0.0", 2026), HealthHandler)
+    print("Python app running on port 2026")
+    server.serve_forever()
 
 if __name__ == "__main__":
-    main()
+    run()
